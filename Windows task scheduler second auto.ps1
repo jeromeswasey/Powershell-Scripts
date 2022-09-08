87879835cc26444f8ae3aaa2a484dc69ff421973
jeromeswasey <105822554+jeromeswasey@users.noreply.github.com>
2022-09-08 14:20:07 -0400

    Add files via upload
    Windows task scheduler script daily (specified time) to execute a batch script that calls the powershell file (ps1) in which you want to execute.

---
+++ Windows task scheduler second auto.ps1
@@ -0,0 +1,5 @@
+﻿$Action = New-ScheduledTaskAction -Execute 'C:\temp\Run Second Auto.bat'
+
+$trigger = New-ScheduledTaskTrigger -Daily -At 2:03pm
+
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "SFTPAutoUpload" -Description "Auto Upload from local directory to remote directory"
